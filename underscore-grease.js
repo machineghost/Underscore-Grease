9d02c3f22db09898e9b650ca3731f491b10216da
Jeremy Walker <jeremy@syapse.com>
2013-02-04 09:57:23 -0800

    Added Underscore Grease to the repo

---
+++ underscore-grease.js
@@ -0,0 +1,21 @@
+/**
+ * Underscore Grease
+ *
+ * https://github.com/machineghost/Underscore-Grease
+ *
+ * Underscore's chaining syntax is kind of ... heavy:
+ *      _(foo).chain().method1().method2().value();
+ *
+ * This mix-in lightens that syntax to simply be:
+ *      _(foo)._method1().method2();
+ * replacing .chain() and .value() with a single extra "_" (well, technically
+ * one "_" per chained method).
+ */
+var greaseMixin = _(_).chain().methods().reduce(function(memo, methodName) {
+    if (methodName == 'chain') return memo; // No point in making a "chain_"
+    memo[methodName + '_'] = function() {
+        return _(_[methodName].apply(this, arguments));
+    };
+    return memo;
+}, {}).value();
+_.mixin(greaseMixin);
